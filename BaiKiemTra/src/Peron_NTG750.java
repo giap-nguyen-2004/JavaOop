5718aa48fca8c161653db724ae06e90c51541210
giap-nguyen-2004 <you@example.com>
2024-05-22 09:44:10 +0700

    NTG750_Cau2

--- BaiKiemTra/src/Peron_NTG750.java
+++ BaiKiemTra/src/Peron_NTG750.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Peron_NTG750 {
     public int tuoi;
     public String HoTen;
     public Float Luong;
+
+    public void Nhap_thong_Tin(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap tuoi Person:");
+        tuoi=sc.nextInt();
+        System.out.println("Nhap ho va ten person:");
+        HoTen=sc.nextLine();
+        System.out.println("Nhap tien Luong cua Person:");
+        Luong=sc.nextFloat();
+    }
+    
+    public void In_thong_tin(){
+        System.out.println("Ho ten:"+HoTen);
+        System.out.println("Tuoi:"+tuoi);
+        System.out.println("Luong:"+Luong);
+    }
 }
